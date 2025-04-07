8cc9c81f6fea91b865ad2edd0b170c4b207ce58e
AGM54 <134453750+AGM54@users.noreply.github.com>
2025-04-07 03:53:58 -0600

    cambios y agregación de rutas

--- BACK/backend/src/app.module.ts
+++ BACK/backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { FileModule } from './file/file.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, FileModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
